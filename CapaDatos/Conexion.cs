61b5210296b0caa0d153b6ec33bc9fff74b3f65c
Jose Ordonez <joseaordoez@gmail.com>
2021-11-17 15:28:58 -0600

    Agregar archivos de proyecto.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    class Conexion
+    {
+        public static string Cn = "Data Source=DESKTOP-JF3IPIM; Initial Catalog=dbventas; Integrated Security=true";
+    }
+}
