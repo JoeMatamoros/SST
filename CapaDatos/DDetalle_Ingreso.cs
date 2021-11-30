d566e7600e9b3ac397c782c137b251fa7ae7167f
Jose Ordonez <joseaordoez@gmail.com>
2021-11-29 21:56:46 -0600

    Preparación compras-almacén

---
+++ CapaDatos/DDetalle_Ingreso.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DDetalle_Ingreso
+    {
+        private int _IdDetalle_Ingreso;
+        private int _Idingreso;
+        private int _Idarticulo;
+        private decimal _Precio_Compra;
+        private decimal _Precio_Venta;
+        private int _Stock_Inicial;
+        private int _Stock_Actual;
+        private DateTime _Fecha_Produccion;
+        private DateTime _Fecha_Vencimiento;
+
+        public int IdDetalle_Ingreso { get => _IdDetalle_Ingreso; set => _IdDetalle_Ingreso = value; }
+        public int Idingreso { get => _Idingreso; set => _Idingreso = value; }
+        public int Idarticulo { get => _Idarticulo; set => _Idarticulo = value; }
+        public decimal Precio_Compra { get => _Precio_Compra; set => _Precio_Compra = value; }
+        public decimal Precio_Venta { get => _Precio_Venta; set => _Precio_Venta = value; }
+        public int Stock_Inicial { get => _Stock_Inicial; set => _Stock_Inicial = value; }
+        public int Stock_Actual { get => _Stock_Actual; set => _Stock_Actual = value; }
+        public DateTime Fecha_Produccion { get => _Fecha_Produccion; set => _Fecha_Produccion = value; }
+        public DateTime Fecha_Vencimiento { get => _Fecha_Vencimiento; set => _Fecha_Vencimiento = value; }
+
+        public DDetalle_Ingreso()
+        {
+
+        }
+
+        public DDetalle_Ingreso(int iddetalle_Ingreso, int idingreso, int idarticulo, decimal precio_compra, decimal precio_venta, int stock_inicial, int stock_actual, DateTime fecha_produccion, DateTime fecha_vencimiento)
+        {
+            this.IdDetalle_Ingreso = iddetalle_Ingreso;
+            this.Idingreso = idingreso;
+            this.Idarticulo = idarticulo;
+            this.Precio_Compra = precio_compra;
+            this.Precio_Venta = precio_venta;
+            this.Stock_Inicial = stock_inicial;
+            this.Stock_Actual = stock_actual;
+            this.Fecha_Produccion = fecha_produccion;
+            this.Fecha_Vencimiento = fecha_vencimiento;
+        }
+    }
+}
